481870b3166015631b14794afbccdf68631d7b1e
stephen <stephenprahljr@outlook.com>
2024-04-02 10:59:42 -0400

    "hw"

---
+++ calc.py
@@ -0,0 +1,9 @@
+def printName(name):
+    return "Hello Mr/Ms {name}...we've been waiting for you!"
+    
+
+def calculate_square_footage(length, width):
+    return length * width
+
+def calculate_circumference(radius):
+    return 2 * 3.14159 * radius
